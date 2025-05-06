1b31c03f76174d898cb895e7043d9e53d9ea6247
Brandon Crabtree <bcrabtree@technicavivunt.com>
2025-05-06 19:43:32 -0400

    Update Technica-OSDCloud.ps1
    Try no Prompt

--- Technica-OSDCloud.ps1
+++ Technica-OSDCloud.ps1
@@ -3,28 +3,26 @@ cls
 $manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
 $model = (Get-CimInstance -ClassName Win32_ComputerSystem).Model
 $serial = (Get-CimInstance -ClassName Win32_BIOS).SerialNumber
-
-Write-Host "$manufacturer"
-Write-Host "$model"
-Write-Host "$serial"
-Import-Module OSD -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
-Install-Module OSD -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
-Write-Host "===================== Main Menu ======================="
-Write-Host "======================================================="
-Write-Host "1: Zero-Touch Win11 24H2 | English | Enterprise"
-Write-Host "1: Zero-Touch Win11 24H2 | English | Pro"
-Write-Host "3: I'll select it myself"
-Write-Host "4: Exit`n"
-$input = Read-Host "Please make a selection"
+# Import-Module OSD -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
+# Install-Module OSD -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
+# Write-Host "===================== Main Menu ======================="
+# Write-Host "======================================================="
+# Write-Host "1: Zero-Touch Win11 24H2 | English | Enterprise"
+# Write-Host "1: Zero-Touch Win11 24H2 | English | Pro"
+# Write-Host "3: I'll select it myself"
+# Write-Host "4: Exit`n"
+# $input = Read-Host "Please make a selection"
 Write-Host "Loading OSDCloud..."
 $OSDModuleResource.StartOSDCloudGUI.BrandName = "Serial Number - $serial"
 $OSDModuleResource.StartOSDCloudGUI.ComputerManufacturer = "$manufacturer"
 $OSDModuleResource.StartOSDCloudGUI.ComputerModel = "$model"
 $OSDModuleResource.StartOSDCloudGUI.updateFirmware = $true
-switch ($input)
-{
-    '1' { Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 24H2 x64' -OSEdition Enterprise -ZTI -Firmware} 
-    '2' { Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 24H2 x64' -OSEdition Pro -ZTI -Firmware} 
-    '3' { Start-OSDCloudGUI } 
-    default { Exit }
-}
+Start-OSDCloudGUI
+
+# switch ($input)
+# {
+#    '1' { Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 24H2 x64' -OSEdition Enterprise -ZTI -Firmware} 
+#    '2' { Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 24H2 x64' -OSEdition Pro -ZTI -Firmware} 
+#    '3' { Start-OSDCloudGUI } 
+#    default { Exit }
+# }
