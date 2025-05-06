6a1f2db23cc01c4673e83945d591207436febff9
Brandon Crabtree <bcrabtree@technicavivunt.com>
2025-05-06 19:11:58 -0400

    Update Technica-OSDCloud.ps1
    Update Branding Test

--- Technica-OSDCloud.ps1
+++ Technica-OSDCloud.ps1
@@ -2,13 +2,11 @@ Write-Host "Starting TechnicaVivunt Custom OSDCloud ..."
 cls
 $manufacturer = (Get-CimInstance -ClassName Win32_ComputerSystem).Manufacturer
 $model = (Get-CimInstance -ClassName Win32_ComputerSystem).Model
-$ipAddress = (Get-NetIPAddress -AddressFamily IPv4 -InterfaceAlias (Get-NetAdapter | Where-Object {$_.Status -eq "Up"} | Select-Object -First 1).InterfaceAlias).IPAddress
 $serial = (Get-CimInstance -ClassName Win32_BIOS).SerialNumber
 
 Write-Host "$manufacturer"
 Write-Host "$model"
 Write-Host "$serial"
-Write-Host "$ipAddress"
 
 Write-Host "===================== Main Menu ======================="
 Write-Host "======================================================="
@@ -18,14 +16,14 @@ Write-Host "3: I'll select it myself"
 Write-Host "4: Exit`n"
 $input = Read-Host "Please make a selection"
 Write-Host "Loading OSDCloud..."
-
 Import-Module OSD -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
 Install-Module OSD -Force -ErrorAction SilentlyContinue -ProgressAction SilentlyContinue
+$OSDModuleResource.StartOSDCloudGUI.BrandName = "$manufacturer $model"
 
 switch ($input)
 {
     '1' { Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 24H2 x64' -OSEdition Enterprise -ZTI -Firmware} 
     '2' { Start-OSDCloud -OSLanguage en-us -OSName 'Windows 11 24H2 x64' -OSEdition Pro -ZTI -Firmware} 
-    '3' { Start-OSDCloudGUI -Firmware } 
+    '3' { Start-OSDCloudGUI -Firmware} 
     default { Exit }
 }
